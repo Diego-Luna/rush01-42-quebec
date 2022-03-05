43c2a6f7d3e19a0978190c6142a839092a770070
Diego francisco Luna lopez <dluna-lo@c1r3p6.42quebec.com>
2022-03-05 10:53:06 -0500

    create 2 tables, with 2 dimensions, one will be the reference and the other the result

--- Diego/main.c
+++ Diego/main.c
@@ -6,7 +6,7 @@
 /*   By: dluna-lo <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/05 10:42:09 by dluna-lo          #+#    #+#             */
-/*   Updated: 2022/03/05 10:47:18 by dluna-lo         ###   ########.fr       */
+/*   Updated: 2022/03/05 10:52:29 by dluna-lo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 int	main(int arg, char **str)
 {
+	int	table_reference[4][4];
+	int	table_result[4][4];
+
 	if (arg == 2)
 	{
 		printf("Value str:%s\n", str[1]);
