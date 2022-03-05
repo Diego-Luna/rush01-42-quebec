4858f6dc116f4ee91638670eecfbe5aa687e5ed4
rachelruro <101009095+rachelruro@users.noreply.github.com>
2022-03-05 15:53:35 -0500

    Add files via upload
    Function ft_put_table with malloc, and printing table showing 0 values (but actually it's only a value for show because malloc is saving the space there - or so I hope).

---
+++ Rachel/ft_table_zero.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_table_zero.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raruiz-r <marvin@42quebec.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/05 11:45:59 by raruiz-r          #+#    #+#             */
+/*   Updated: 2022/03/05 15:50:09 by raruiz-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include<unistd.h>
+#include<stdlib.h>
+#define SIZE_RES 4
+
+void	ft_put_table(int x, int y);
+
+void	ft_table_zero(int x, int y)
+{
+	int	*table_result[SIZE_RES][SIZE_RES];
+
+	table_result[x][y] = malloc(SIZE_RES * SIZE_RES * sizeof(int));
+	x = 0;
+	while (x < SIZE_RES)
+	{
+		y = 0;
+		while (y < SIZE_RES)
+		{
+			y++;
+		}
+		x++;
+	}	
+	ft_put_table(x, y);
+}
+
+void	ft_put_table(int x, int y)
+{
+	int	x_table;
+	int	y_table;
+	int	z;
+
+	z = '0';
+	if (x <= 0 || y <= 0)
+	{
+		return ;
+	}
+	y_table = 0;
+	x_table = 0;
+	while (++y_table <= y)
+	{
+		while (++x_table <= x)
+		{
+			write (1, &z, 1);
+		}
+		x_table = 0;
+		write (1, "\n", 1);
+	}
+}
+
+int	main(void)
+{
+	int	x;
+	int	y;
+
+	x = 4;
+	y = 4;
+	ft_table_zero(x, y);
+}
