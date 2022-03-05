ff9cfc5f0519ba544a96c0f3fef4207d4fac4a3e
Diego francisco Luna lopez <dluna-lo@c1r3p6.42quebec.com>
2022-03-05 13:35:29 -0500

    norminette ready

--- Diego/main.c
+++ Diego/main.c
@@ -6,7 +6,7 @@
 /*   By: dluna-lo <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/05 10:42:09 by dluna-lo          #+#    #+#             */
-/*   Updated: 2022/03/05 13:23:01 by dluna-lo         ###   ########.fr       */
+/*   Updated: 2022/03/05 13:34:04 by dluna-lo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,8 @@ int	main(int arg, char **str)
 	int	table_reference[SIZE_RES + 2][SIZE_RES + 2];
 	int	table_result[SIZE_RES][SIZE_RES];
 
-	if (arg == 2 && ft_strlen(str[1]) == 31 && ft_str_is_numeric_and_space(str[1]))
+	if (arg == 2 && ft_strlen(str[1]) == 31
+		&& ft_str_is_numeric_and_space(str[1]))
 	{
 		printf("Value str:%s\n", str[1]);
 	}
--- Diego/string.c
+++ Diego/string.c
@@ -6,7 +6,7 @@
 /*   By: dluna-lo <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/05 10:54:22 by dluna-lo          #+#    #+#             */
-/*   Updated: 2022/03/05 13:23:04 by dluna-lo         ###   ########.fr       */
+/*   Updated: 2022/03/05 13:34:02 by dluna-lo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_strlen(char *str)
 	return (i);
 }
 
-int ft_number(char str)
+int	ft_number(char str)
 {
 	if (str >= '1' && str <= '4')
 	{
@@ -43,17 +43,11 @@ int	ft_str_is_numeric_and_space(char *str)
 		while (str[index] != '\0')
 		{
 			if (index == 0 && !(ft_number(str[index])))
-			{
 				return (0);
-			}
 			else if (str[index - 1] == ' ' && !(ft_number(str[index])))
-			{
 				return (0);
-			}
 			else if (str[index] == ' ' && !(ft_number(str[index - 1])))
-			{
 				return (0);
-			}
 			index++;
 		}
 		return (1);
