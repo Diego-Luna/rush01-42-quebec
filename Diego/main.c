e94c1bcc2f11fa86a48aaffe01c3391d8fc6a5a4
Diego francisco Luna lopez <dluna-lo@c1r3p6.42quebec.com>
2022-03-05 13:23:43 -0500

    add diego, 2, 1/2

--- Diego/main.c
+++ Diego/main.c
@@ -6,18 +6,23 @@
 /*   By: dluna-lo <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/05 10:42:09 by dluna-lo          #+#    #+#             */
-/*   Updated: 2022/03/05 11:31:06 by dluna-lo         ###   ########.fr       */
+/*   Updated: 2022/03/05 13:23:01 by dluna-lo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
+int	ft_strlen(char *str);
+int	ft_str_is_numeric_and_space(char *str);
+
+#define SIZE_RES 4
+
 int	main(int arg, char **str)
 {
-	int	table_reference[5][5];
-	int	table_result[4][4];
+	int	table_reference[SIZE_RES + 2][SIZE_RES + 2];
+	int	table_result[SIZE_RES][SIZE_RES];
 
-	if (arg == 2)
+	if (arg == 2 && ft_strlen(str[1]) == 31 && ft_str_is_numeric_and_space(str[1]))
 	{
 		printf("Value str:%s\n", str[1]);
 	}
---
+++ Diego/string.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   string.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dluna-lo <marvin@42quebec.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/05 10:54:22 by dluna-lo          #+#    #+#             */
+/*   Updated: 2022/03/05 13:23:04 by dluna-lo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+int ft_number(char str)
+{
+	if (str >= '1' && str <= '4')
+	{
+		return (1);
+	}
+	return (0);
+}
+
+int	ft_str_is_numeric_and_space(char *str)
+{
+	int	index;
+
+	if (str[0] == '\0')
+	{
+		return (1);
+	}
+	else
+	{
+		index = 0;
+		while (str[index] != '\0')
+		{
+			if (index == 0 && !(ft_number(str[index])))
+			{
+				return (0);
+			}
+			else if (str[index - 1] == ' ' && !(ft_number(str[index])))
+			{
+				return (0);
+			}
+			else if (str[index] == ' ' && !(ft_number(str[index - 1])))
+			{
+				return (0);
+			}
+			index++;
+		}
+		return (1);
+	}
+}
