50ad49d4acdc3ddc26042782d55fffce77ab98d4
Diego francisco Luna lopez <dluna-lo@c1r3p6.42quebec.com>
2022-03-06 23:00:54 -0500

    no completo

--- Diego/ft_table_logic.c
+++ Diego/ft_table_logic.c
@@ -6,11 +6,199 @@
 /*   By: dluna-lo <dluna-lo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/06 12:08:42 by dluna-lo          #+#    #+#             */
-/*   Updated: 2022/03/06 21:12:24 by dluna-lo         ###   ########.fr       */
+/*   Updated: 2022/03/06 22:58:23 by dluna-lo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+/*
+void ft_if_1_3_3(int *find, int value_check)
+{
+	if (find == 0)
+	{
+		if (value_check == 1)
+		{
+			find = 1;
+			i = 0;
+		}
+	}
+	if (find == 1)
+	{
+		if (value_check == 3)
+		{
+				find = 2;
+		}
+	}
+	if (find == 2)
+	{		
+		if (value_check == 3)
+		{
+			find = 3;
+		}
+	}
+}
+
+int	ft_1_3_3_v(int t_r[6][6], int x)
+{
+	int i;
+
+	int find = 0;
+	i = 1;
+	while (i < 5)
+	{
+		ft_if_1_3_3(&find, t_r[i][x])
+		i++;
+	}
+
+	if (find == 3)
+	{
+		return (1);
+	}
+	else (0);
+}
+
+int	ft_1_3_3_h(int t_r[6][6], int y)
+{
+	int i;
+
+	int find = 0;
+	i = 1;
+	while (i < 5)
+	{
+		ft_if_1_3_3(&find, t_r[y][i])
+		i++;
+	}
+
+	if (find == 3)
+	{
+		return (1);
+	}
+	else (0);
+}
+
+int ft_check_1_3_3(int t_r[6][6], int results[4])
+{
+	int	x;
+	int	y;
+	y = 0;
+	
+	if (ft_1_3_3_h(t_r, 0))
+		results[0] = 1;
+	if (ft_1_3_3_v(t_r, 0))
+		results[1] = 2;
+	if (ft_1_3_3_v(t_r, 5))
+		results[2] = 3;
+	if (ft_1_3_3_h(t_r, 5))
+		results[3] = 4;
+}
+
+int	ft_index_number_not_013_h(int t_r[6][6], int y)
+{
+	int i;
+
+	i = 0;
+	while (i < 4)
+	{
+		if (t_r[y][i] == 2)
+		{
+			return i;
+		}
+		i++;
+	}
+}
+
+int	ft_index_number_not_013_v(int t_r[6][6], int x)
+{
+	int i;
+
+	i = 0;
+	while (i < 4)
+	{
+		if (t_r[i][x] == 2)
+		{
+			return i;
+		}
+		i++;
+	}
+}
+
+void ft_change_value_3(int t_r[6][6], int table[4][4], int orientation)
+{
+	int index;
+
+	if (orientation == 1)
+	{
+		index = ft_index_number_not_013_h();
+	}
+}
+
+void ft_reference_1_3_3(int t_r[6][6])
+{
+	int	results[4];
+	int	i;
+	ft_check_1_3_3(t_r, results);
+	while (i < 4)
+	{
+		if (results[i] != 0)
+			ft_change_value_3(t_r, table, orientation);
+	}
+}
+*/
+int	ft_number_h(int t[4][4], int number, int x, int y)
+{
+	int	i;
+
+	i = 0;
+	while(i < 4)
+	{
+		if (number == t[y][i] && i != x)
+		{
+			return (0);
+		}
+		i++;
+	}
+
+	return (1);
+} 
+
+int	ft_number_v(int t[4][4], int number, int x, int y)
+{
+	int	i;
+
+	i = 0;
+	while(i < 4)
+	{
+		if (number == t[i][x] && y != x)
+		{
+			return (0);
+		}
+		i++;
+	}
+
+	return (1);
+} 
+
+void	ft_complete_number(int t[4][4], int number)
+{
+	int x;
+	int y;
+
+	y = 0;
+	while (y < 4)
+	{	
+		x = 0;
+		while (x < 4)
+		{
+			if (ft_number_h(t, number, x, y) && ft_number_v(t, number, x, y))
+			{
+				t[y][x] = number;
+			}
+		x++;
+		}
+	y++;
+	}
+}
+
 void	ft_number_1_4_v(int t[4][4],int x, int y)
 {
 	int i;
@@ -101,5 +289,6 @@ void	ft_check_4(int t[4][4],int t_r[6][6])
 
 void	ft_run_login(int table[4][4],int table_reference[6][6])
 {
-	ft_check_4(table, table_reference);	
+	ft_check_4(table, table_reference);
+	ft_complete_number(table, 4);
 }
